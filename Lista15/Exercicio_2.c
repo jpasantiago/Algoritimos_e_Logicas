f527d9d26aff02978e8062a96f8c31fa3af77782
jpasantiago <131825902+jpasantiago@users.noreply.github.com>
2023-04-25 21:52:02 -0300

    lista 15

---
+++ Lista15/Exercicio_2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <conio.h>
+
+/*2 - Escreva um algoritmo que leia seu nome e após a leitura e exiba o nome lido..*/
+
+void main (){
+	char name ;
+	printf ("Ola Mundo! ") ;
+	system ("pause" );
+}
